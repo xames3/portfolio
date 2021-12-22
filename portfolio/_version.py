cfc658d554d3a5a953e3edb88f0a5d13df2c74ff
xames3 <xa@mes3.in>
2021-12-23 02:28:23 +0530

    Upstream version on remote

---
+++ portfolio/_version.py
@@ -0,0 +1,11 @@
+"""
+Changelog and version history for portfolio application.
+
+..versionchanged:: 0.0.1
+    Built and setup the structure for the application. Added the basic
+    global level module like logging, exceptions and api structure.
+"""
+
+__all__ = ["__version__"]
+
+__version__ = "0.0.1"
